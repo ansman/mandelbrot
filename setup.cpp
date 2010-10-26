c596dc6028dc20574c1f83fa9fc8c60cf2db20b9
Emil Arfvidsson <emil@arfvidsson.com>
2010-10-26 21:36:18 +0200

    Inlined the kernel and replaced long double with double.

--- setup.cpp
+++ setup.cpp
@@ -9,6 +9,32 @@ cl_kernel kernel;
 cl_mem input;
 cl_mem output;
 
+const char *KernelSource = "\n" \
+"__kernel mandelcalc(double xs, double ys, double stepx, double stepy, unsigned int width, unsigned int height, unsigned int iterations, __global float * output)                                                       \n" \
+"{                                                                                       \n" \
+"    int index = get_global_id(0);                                                       \n" \
+"	                                                                                     \n" \
+"    double x0 = xs + stepx*(double)(index%width);                                       \n" \
+"    double y0 = ys + stepy*(double)(index%height);                                      \n" \
+"                                                                                        \n" \
+"    double x = 0;                                                                       \n" \
+"	 double y = 0;                                                                       \n" \
+"    unsigned int it = 0;                                                                \n" \
+"	                                                                                     \n" \
+"    while (it < iterations) {                                                           \n" \
+"       double xx = x*x;                                                                 \n" \
+"		double yy = y*y;                                                                 \n" \
+"        if (xx+yy > 4)                                                                  \n" \
+"            break;                                                                      \n" \
+"        y = 2*x*y + y0;                                                                 \n" \
+"        x = xx - yy + x0;                                                               \n" \
+"        ++it;                                                                           \n" \
+"    }                                                                                   \n" \
+"	                                                                                     \n" \
+"    output[index] = it/iterations;                                                      \n" \
+"}                                                                                       \n" \
+"\n";
+
 void setup() {
 	int err;
 	
@@ -30,25 +56,25 @@ void setup() {
 		exit(1);
 	}
 	
-	std::ifstream in;
-	std::stringstream s;
+//	std::ifstream in;
+//	std::stringstream s;
+//	
+//	in.open(programFile, std::ios_base::binary);
+//	
+//	if(in.fail()) {
+//		std::cerr << "Error: Could not open program code from " << programFile  << std::endl;
+//		exit(1);
+//	}
+//	
+//	while(!in.eof() && in.peek() != -1) {
+//		s.put((char)in.get());
+//	}
+//	
+//	in.close();
+//		
+//	const char * programCodeString = s.str().c_str();
 	
-	in.open(programFile, std::ios_base::binary);
-	
-	if(in.fail()) {
-		std::cerr << "Error: Could not open program code from " << programFile  << std::endl;
-		exit(1);
-	}
-	
-	while(!in.eof() && in.peek() != -1) {
-		s.put((char)in.get());
-	}
-	
-	in.close();
-		
-	const char * programCodeString = s.str().c_str();
-	
-	program = clCreateProgramWithSource(context, 1, (const char **) &programCodeString, NULL, &err);
+	program = clCreateProgramWithSource(context, 1, (const char **) &KernelSource, NULL, &err);
 	if (!program) {
 		std::cerr << "Error: Failed to create compute program!" << std::endl;
 		exit(1);
